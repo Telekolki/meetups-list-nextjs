df2d34b60f788660e83ac06aaa7cf546a3107559
Gribs <telekolki@gmail.com>
2022-09-18 20:10:24 +0300

    event content added

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,9 +1,31 @@
 import { useRouter } from 'next/router';
-
+import { Fragment } from 'react';
+import { getEventById } from '../../dummy-data';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventContent from '../../components/event-detail/event-content';
+import EventLogistics from '../../components/event-detail/event-logistics';
 const Ids = () => {
   const router = useRouter();
+  const event = getEventById(router.query['id']);
 
-  return <div>ID page {router.query['id']}</div>;
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
 };
 
 export default Ids;
